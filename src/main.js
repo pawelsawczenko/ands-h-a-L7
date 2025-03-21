af6803a0fb317e2870141968432ad77097cb48c3
pawelsawczenko <pawelsawczenko@gmail.com>
2025-03-21 17:17:48 +0300

    add h/a 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 import "./style.css";
+//
+// -- H / A - L6 -- 1
+//
+
+const runawayBtn = document.querySelector(".runaway-btn");
+
+runawayBtn.style.left = "50%";
+runawayBtn.style.top = "50%";
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+function randomizeBtnPosition() {
+  runawayBtn.style.left = `${randomInteger(0, 100)}%`;
+  runawayBtn.style.top = `${randomInteger(0, 100)}%`;
+}
+
+runawayBtn.addEventListener("mouseover", () => {
+  if (Math.random() < 0.5) {
+    randomizeBtnPosition();
+  }
+});
+runawayBtn.addEventListener("click", () => {
+  randomizeBtnPosition();
+});
