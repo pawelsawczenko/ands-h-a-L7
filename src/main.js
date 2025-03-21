e5696227526b0a33dc5c19d6c6f3a6218eae50fb
pawelsawczenko <pawelsawczenko@gmail.com>
2025-03-21 19:44:55 +0300

    add h/a 2

--- src/main.js
+++ src/main.js
@@ -26,3 +26,97 @@ runawayBtn.addEventListener("mouseover", () => {
 runawayBtn.addEventListener("click", () => {
   randomizeBtnPosition();
 });
+
+//
+// -- H / A - L6 -- 2 -- stopwatch
+//
+
+const stopwatchItem = document.querySelector(".stopwatch");
+
+const markup = `
+          <div id="stopwatch-time">
+            <span id="stopwatch-hr">00</span>
+            <span>&#58;</span>
+            <span id="stopwatch-min">00</span>
+            <span>&#58;</span>
+            <span id="stopwatch-sec">00</span>
+          </div>
+          <div id="stopwatch-buttons">
+            <button class="btn" id="stopwatch-start">Start</button>
+            <button class="btn" disabled id="stopwatch-stop">Stop</button>
+            <button class="btn" id="stopwatch-reset">Reset</button>
+          </div>
+`;
+
+stopwatchItem.insertAdjacentHTML("afterbegin", markup);
+
+const startBtn = document.querySelector("#stopwatch-start");
+const stopBtn = document.querySelector("#stopwatch-stop");
+const resetBtn = document.querySelector("#stopwatch-reset");
+
+const stopwatchHour = document.querySelector("#stopwatch-hr");
+const stopwatchMin = document.querySelector("#stopwatch-min");
+const stopwatchSec = document.querySelector("#stopwatch-sec");
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+
+function stopwatch() {
+  second++;
+
+  if (second === 60) {
+    minute++;
+    second = 0;
+  }
+
+  if (minute === 60) {
+    hour++;
+    minute = 0;
+    second = 0;
+  }
+
+  let hrStr = hour < 10 ? "0" + hour : hour;
+  let minStr = minute < 10 ? "0" + minute : minute;
+  let secStr = second < 10 ? "0" + second : second;
+
+  // console.log(`${hrStr}:${minStr}:${secStr}`);
+  // console.log("tick");
+
+  stopwatchHour.innerHTML = hrStr;
+  stopwatchMin.innerHTML = minStr;
+  stopwatchSec.innerHTML = secStr;
+}
+
+let stopwatchID;
+
+startBtn.addEventListener("click", () => {
+  stopwatchID = setInterval(stopwatch, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(stopwatchID);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(stopwatchID);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  stopwatchHour.innerHTML = "00";
+  stopwatchMin.innerHTML = "00";
+  stopwatchSec.innerHTML = "00";
+  hour = 0;
+  minute = 0;
+  second = 0;
+});
+
+//
+// -- H / A - L6 -- 2 -- timer
+//
